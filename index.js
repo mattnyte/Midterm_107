445d814eefc65f26561e9c311a5e07dc18694021
mattnyte <162880062+mattnyte@users.noreply.github.com>
2024-03-10 09:39:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,22 +12,23 @@ function updateOpening (index) {
     document.getElementById("opening-about").textContent = openingData["opening-about"];
     document.getElementById("opening-players").textContent = openingData["opening-players"];
 }
-
-let jsonData;
-fetch('data.json')
-    .then(response => response.json())
-    .then (data => {
-        jsonData = data;
-        if (window.location.pathname.includes("index.html")) {
-            updateStyle(0);
-            document.getElementById("aggressive").addEventListener("click", function() {updateStyle(1);});
-            document.getElementById("solid").addEventListener("click", function() {updateStyle(2);});
-            document.getElementById("overall").addEventListener("click", function() {updateStyle(3);});
-        }
-        if (window.location.pathname.includes("openings.html")) {
-            updateOpening(0)
-            document.getElementById("kings-gambit").addEventListener("click", function() {updateOpening(1);});
-            document.getElementById("queens-gambit").addEventListener("click", function() {updateOpening(2);});
-            document.getElementById("ruy-lopez").addEventListener("click", function() {updateOpening(3);});
-        }
-    })
+document.addEventListener("DOMContentLoaded", function() {
+    let jsonData;
+    fetch('data.json')
+        .then(response => response.json())
+        .then (data => {
+            jsonData = data;
+            if (window.location.pathname.includes("index.html")) {
+                updateStyle(0);
+                document.getElementById("aggressive").addEventListener("click", function() {updateStyle(1);});
+                document.getElementById("solid").addEventListener("click", function() {updateStyle(2);});
+                document.getElementById("overall").addEventListener("click", function() {updateStyle(3);});
+            }
+            if (window.location.pathname.includes("openings.html")) {
+                updateOpening(0)
+                document.getElementById("kings-gambit").addEventListener("click", function() {updateOpening(1);});
+                document.getElementById("queens-gambit").addEventListener("click", function() {updateOpening(2);});
+                document.getElementById("ruy-lopez").addEventListener("click", function() {updateOpening(3);});
+            }
+        });
+});
