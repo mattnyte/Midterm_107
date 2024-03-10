4b17033ef125fb370a5f82832a043e44373b0a11
mattnyte <162880062+mattnyte@users.noreply.github.com>
2024-03-10 07:02:41 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ function updateStyle (index) {
     document.getElementById("style-about").textContent = styleData["style-about"];
     document.getElementById("opening-type").textContent = styleData["opening-type"];
 }
+function updateOpening (index) {
+    const styleData = jsonData.styleData[index];
+    document.getElementById("opening-name").textContent = styleData["opening-name"];
+    document.getElementById("opening-picture").querySelector("img").src = styleData["opening-picture"];
+    document.getElementById("opening-about").textContent = styleData["opening-about"];
+    document.getElementById("opening-players").textContent = styleData["opening-players"];
+}
 
 let jsonData;
 fetch('data.json')
     .then(response => response.json())
-    .then (data => {jsonData = data
-        updateStyle(0);
-        document.getElementById("aggressive").addEventListener("click", function() {updateStyle(1);});
-        document.getElementById("solid").addEventListener("click", function() {updateStyle(2);});
-        document.getElementById("overall").addEventListener("click", function() {updateStyle(3);});
+    .then (data => {
+        jsonData = data;
+        if (window.location.pathname.includes("index.html")) {
+            updateStyle(0);
+            document.getElementById("aggressive").addEventListener("click", function() {updateStyle(1);});
+            document.getElementById("solid").addEventListener("click", function() {updateStyle(2);});
+            document.getElementById("overall").addEventListener("click", function() {updateStyle(3);});
+        }
+        if (window.location.pathname.includes("openings.html")) {
+            updateOpening(0)
+            document.getElementById("kings-gambit").addEventListener("click", function() {updateOpening(1);});
+            document.getElementById("queens-gambit").addEventListener("click", function() {updateOpening(2);});
+            document.getElementById("ruy-lopez").addEventListener("click", function() {updateOpening(3);});
+        }
     })
